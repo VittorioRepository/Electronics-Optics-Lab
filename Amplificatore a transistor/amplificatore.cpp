995ebcae323599954cbbfb481742fd5d64b72ecd
VittorioRepository <vittorioguidetti2001@gmail.com>
2022-05-21 17:04:36 +0200

    Added Transistor Amplifier analysis.

--- Amplificatore a transistor/amplificatore.cpp
+++ Amplificatore a transistor/amplificatore.cpp
@@ -1,14 +1,14 @@
 /* 
 
-MALUS
+AMPLIFICATORE
 
 */ 
 
 
-void malus()
+void amplificatore()
 {
     char filename[100];
-    sprintf(filename, "malus.txt");
+    sprintf(filename, "data.txt");
 
     // LETTURA DATI
 
@@ -31,29 +31,32 @@ void malus()
         ++npoints;
     }
 
-    npoints = npoints/2;
-    cout << "\nSono state raccolte: " << npoints << " misure di coppie intensità - angolo" << endl;
+    npoints = npoints/3;
+    cout << "\nSono state raccolte: " << npoints << " misure di coppie guadagno - frequenza" << endl;
 
     // Lettura 
 
     rewind(input);
-    float *I = new float[npoints];
-    float *theta = new float[npoints];
-    float *err_I = new float[npoints];
-    float *err_theta = new float[npoints];
+    float *f = new float[npoints];
+    float *err_f = new float[npoints];
+    float *Av = new float[npoints];
+    float *err_Av = new float[npoints];
 
     int i=0;
     while(!feof(input))
     {
         if(i<npoints){
             fscanf(input,"%f\n",&temp_val);
-            theta[i] = temp_val;
-            err_theta[i] = 2;
+            f[i] = temp_val;
+            err_f[i] = 1/1000*f[i];
         }
         else if(i<2*npoints){
             fscanf(input,"%f\n",&temp_val);
-            I[i-npoints] = temp_val;
-            err_I[i-npoints] = I[i-npoints]*(0.5/100)+0.1;
+            Av[i-npoints] = temp_val;
+        }
+        else if(i<3*npoints){
+            fscanf(input,"%f\n",&temp_val);
+            err_Av[i-2*npoints] = temp_val;
         }
         ++i;
         
@@ -76,7 +79,7 @@ void malus()
     {
         cout << "VALORI" << endl;
         for(int j=0;j<npoints-1;j++){
-            cout << theta[j] << ": " << I[j] << " +- " << err_I[j] << endl;
+            cout << f[j] << " +- " << err_f[j] << " : " << Av[j] << " +- " << err_Av[j] << endl;
         }
 
     }
@@ -86,21 +89,21 @@ void malus()
 
     // PLOT E FIT
 
-    TCanvas *I_theta = new TCanvas("Malus","Malus",200,10,1000,700);
+    TCanvas *I_theta = new TCanvas("Amplificatore","Amplificatore a Trasnsistor",200,10,1000,700);
     I_theta->SetFillColor(0);
     I_theta->cd();
-    TGraphErrors *g_it = new TGraphErrors(npoints,theta,I,err_theta,err_I);
+    TGraphErrors *g_it = new TGraphErrors(npoints,f,Av,err_f,err_Av);
 
     // Abbellimenti 
     g_it->SetMarkerSize(0.6);
     g_it->SetMarkerStyle(21);
-    g_it->SetTitle("I(#theta)"); // Titolo del grafico
-    g_it->GetYaxis()->SetTitle("Corrente I [#mu A]"); // Titoli degli assi
-    g_it->GetXaxis()->SetTitle("Angolo #theta");
+    g_it->SetTitle("Av(f)"); // Titolo del grafico
+    g_it->GetYaxis()->SetTitle("Guadagno Av"); // Titoli degli assi
+    g_it->GetXaxis()->SetTitle("Frequenza [Hz]");
 
     //Per impostare scala logaritmica:
     //su asse x:
-    //I_theta->SetLogx(1);
+    I_theta->SetLogx(1);
     //su asse y:
     //I_theta->SetLogy(1);
 
@@ -110,7 +113,7 @@ void malus()
 
     // I(theta)   
 
-    TF1 *function = new TF1("Fit","[0]+[1]*pow(cos(x/180*3.1415926535+[2]),2)",-5,375);
+    TF1 *function = new TF1("Fit","[0]+[1]*pow(x-[2],-1)+[3]*pow(x-[2],-2)",0,1e7);
     
     //function->SetParameter(0,0);
     //function->SetParameter(1,0);
